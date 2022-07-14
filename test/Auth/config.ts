36ed2bc829ad4675f1f10f21f560371822fedc1f
preetgur <preetgur0137@gmail.com>
2022-07-14 16:14:17 +0530

    feat : generate jwt with amplify

---
+++ test/Auth/config.ts
@@ -0,0 +1,7 @@
+export const config = {
+    REGION: 'us-east-1',
+    USER_POOL_ID : 'us-east-1_OPiRj3Bdm',
+    APP_CLIENT_ID :'6l1v7ch17r8lervcoub8lia0sa',
+    TEST_USER_NAME: 'preetgur',
+    TEST_USER_PASSWORD: 'Admin@1313',
+}
